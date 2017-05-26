e25d7310bed473c46e976cb12654c71a5426054c
fifiarisandi <fifi_arisandi@yahoo.com>
2017-05-26 16:19:10 +0800

    Add the function to draw the inner petal.

--- flower_by_turtle.py
+++ flower_by_turtle.py
@@ -8,12 +8,24 @@ def draw_center():
 		ted.seth(angle)
 		ted.circle(20)
 
+def draw_petal1():
+	ted.color("red")
+	ted.pensize(2)
+	for angle in range(0,360,15):
+		ted.seth(angle)
+		ted.fd(100)
+		ted.left(120)
+		ted.fd(100)
+		ted.left(120)
+		ted.fd(100)
+
 
 def drawing_flower():
 	window = turtle.Screen()
 	window.bgcolor("white")
 	ted.speed(10)
 	draw_center()
+	draw_petal1()
 
 	window.exitonclick()
 
